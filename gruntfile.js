fc4affad3486c0589d4fb1be53599a82957fa6b4
root <root@chef-node1.c2clab.com>
2017-08-01 15:34:44 +0530

    gitignore updated

--- gruntfile.js
+++ gruntfile.js
@@ -179,7 +179,7 @@ module.exports = function (grunt) {
       },
       run: {
         command: [
-          'docker rm -f portainer',
+          'docker rm -f portainerr',
           'docker run -d -p 9000:9000 -v $(pwd)/dist:/app -v $(pwd)/userdata:/data -v /var/run/docker.sock:/var/run/docker.sock:z --name portainer portainer/base /app/portainer-linux-amd64 --no-analytics -a /app'
         ].join(';')
       }
